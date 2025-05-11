9b66e82a054668b03408d6c64ae6becd25dae229
4q0c <yujin40401@yahoo.co.jp>
2025-05-11 11:00:37 +0900

    コマンドもうちょい修正。

--- commands/help/index.js
+++ commands/help/index.js
@@ -3,13 +3,12 @@ let gui = "/" + "SUN gui"
 const mesadd = "&b/add <count>: 現在の座標を保存します。<count>は座標のカウントです。"
 const meslis = "&b/list: 保存された座標の一覧を表示します。"
 
-
 export function help() {
     ChatLib.chat("--------------------------------------------------------------")
     ChatLib.chat("&aSUNコマンドの使用方法:")
-    new TextComponent(mesgui).setClick("suggest_command", `${gui}`).chat().setHover("show_text" `&bクリックしてコマンドを貼り付け。`)
-    new TextComponent(mesadd).setClick("run_command", `/helpcommandImsun`).chat().setHover("show_text", `&bクリックして見本を表示。`)
-    new TextComponent(meslis).setClick("run_command", `/sun list`).chat().setHover("show_text", `&bクリックしてコマンドを実行。`)
+    new TextComponent(mesgui).setClick("suggest_command", `${gui}`).setHover("show_text", `&bクリックしてコマンドを貼り付け。`).chat()
+    new TextComponent(mesadd).setClick("run_command", `/helpcommandImsun`).setHover("show_text", `&bクリックして見本を表示。`).chat()
+    new TextComponent(meslis).setClick("run_command", `/sun list`).setHover("show_text", `&bクリックしてコマンドを実行。`).chat()
     ChatLib.chat("&b/delete <X> <Y> <Z>: 指定した座標を削除します。")
     ChatLib.chat("&b/mouselock <true|false> 視点移動を制限します。")
     ChatLib.chat("--------------------------------------------------------------")
